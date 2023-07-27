416b0d6baba5dae35d39308635cc90988ffb3b2f
KienBui2k <haikien371316@gmail.com>
2023-07-27 14:49:22 +0700

    full api

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -2,7 +2,25 @@ import categoryModel from "../modles/category.modle"
 
 
 module.exports = {
-    create: function (req, res) {
-        console.log("da vao controller modle");
+    create: async function (req, res) {
+        console.log("da vao controller modle", req.body);
+        try {
+            let result = await categoryModel.create(req.body)
+            if (result.status) {
+                return res.status(200).json({
+                    message: result.message,
+                    data: result.data
+                })
+            }
+            return res.status(500).json({
+                message: result.message
+            })
+        } catch (err) {
+            return res.status(500).json(
+                {
+                    message: "lôi không xác định !",
+                }
+            )
+        }
     }
 }
